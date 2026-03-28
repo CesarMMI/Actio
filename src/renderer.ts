c83b393de2953f4e4eb15ad983b582e8ec953fb8
cesar <cesargustavo53@gmail.com>
2026-03-28 13:34:02 -0300

    project setup

---
+++ src/renderer.ts
@@ -0,0 +1,7 @@
+import './index.css';
+import { createRoot } from 'react-dom/client';
+import { createElement } from 'react';
+import App from './App';
+
+const root = document.getElementById('root');
+createRoot(root).render(createElement(App));
